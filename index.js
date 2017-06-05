29afd57405a7b0e9824f1577738f60d72ed37f8f
Daniel Bank <daniel.p.bank@gmail.com>
2017-06-05 16:26:31 -0700

    Add express and express-graphql

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
 const { graphql, buildSchema } = require('graphql');
+
+const PORT = process.env.PORT || 3000;
+const server = express();
 
 const schema = buildSchema(`
 type Video {
@@ -46,17 +51,12 @@ const resolvers = {
   videos: () => videos,
 };
 
-const query = `
-query myFirstQuery {
-  videos {
-    id,
-    title,
-    duration,
-    watched
-  }
-}
-`;
+server.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+  rootValue: resolvers,
+}));
 
-graphql(schema, query, resolvers)
-  .then(result => console.log(result))
-  .catch(error => console.log(error));
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
